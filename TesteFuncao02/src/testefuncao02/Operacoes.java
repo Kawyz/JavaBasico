a1cb11e4d3904d995ab82ad2dd0036a28926aecc
Kawyz <kawannacisz04@gmail.com>
2025-04-01 21:46:24 -0300

    +1 exercicio

---
+++ TesteFuncao02/src/testefuncao02/Operacoes.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package testefuncao02;
+
+/**
+ *
+ * @author kakaw
+ */
+public class Operacoes {
+    
+    public static String contador (int inicio, int fim){
+        String s = " ";
+        
+        for (int c = inicio; c <= fim; c++){
+            s += c + " ";
+        }
+        return s;
+    }
+}
